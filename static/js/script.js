fc84215fd7238d8a272a4d614775cc8f35a05979
Fiona-T <fitracey@gmail.com>
2021-10-04 15:22:34 +0000

    add all files to recreate project

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// from Materialize, for mobile sidenav
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    // initialise it
+    M.Sidenav.init(sidenav);
+  });
